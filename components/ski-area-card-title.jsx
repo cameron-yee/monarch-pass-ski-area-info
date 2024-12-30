f3e6f9b2a4c39da5b84f230d862e7a6e01f26106
Cameron Yee <yee.camero23@gmail.com>
2024-12-30 10:19:40 -0700

    fix: adds expand and collapse functionality

--- components/ski-area-card-title.jsx
+++ components/ski-area-card-title.jsx
@@ -1,12 +1,21 @@
 import classnames from 'classnames'
 
-export default function SkiAreaCardTitle({ title }) {
-  const areaTitleClassnames = classnames({ "text-3xl text-zinc-100": true })
+export default function SkiAreaCardTitle({ isExpanded, onClick, title }) {
+  const areaTitleClassnames = classnames({ "text-3xl text-zinc-100 mr-3": true })
   const borderClassnames = classnames({ "block border-4 border-yellow-600 my-4 rounded": true })
+  const toggleClassnames = classnames({
+    "text-gray-300 bg-gray-800 text-xs px-4 py-1 outline-none focus:ring-2 rounded-full ring-yellow-600 border border-gray-500": true
+  })
 
   return (
     <>
-      <h2 className={areaTitleClassnames}>{title}</h2>
+      <div className={'flex'}>
+        <h2 className={areaTitleClassnames}>{title}</h2>
+        {isExpanded
+           ? <button onClick={onClick} className={toggleClassnames}>Hide &#8593;</button>
+           : <button onClick={onClick} className={toggleClassnames}>Show &#8595;</button>
+        }
+      </div>
       <span className={borderClassnames} />
     </>
   )
--- components/ski-area-card.jsx
+++ components/ski-area-card.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useState } from 'react'
 import classnames from 'classnames'
 
 import InfoBlock from './info-block'
@@ -11,11 +12,15 @@ export default function SkiAreaCard({
   filterValues,
   name
 }) {
+  const [isExpanded, setIsExpanded] = useState(true)
+  const toggleIsExpanded = useCallback(() => setIsExpanded((current) => !current), [])
+
   if (data === null) {
     return null
   }
 
   const areaWrapperClassnames = classnames({ "bg-gray-600 p-3 mb-5 rounded shadow-lg": true })
+  const expandableClassNames = classnames({ 'max-h-0 overflow-hidden': !isExpanded })
 
   const { liftInfo, snowInfo } = data
 
@@ -26,19 +31,23 @@ export default function SkiAreaCard({
   if (children) {
     return (
       <div className={areaWrapperClassnames}>
-        <SkiAreaCardTitle title={name} />
-        {children}
+        <SkiAreaCardTitle title={name} onClick={toggleIsExpanded} isExpanded={isExpanded} />
+        <div className={expandableClassNames}>
+          {children}
+        </div>
       </div>
     )
   }
 
   return (
       <div className={areaWrapperClassnames}>
-        <SkiAreaCardTitle title={name} />
-        <SkiAreaCardSubTitle title={'Snow Info'} />
-        <InfoBlock infoBlock={snowInfo} filterValues={filterValues} />
-        <SkiAreaCardSubTitle title={'Lift Info'} />
-        <InfoBlock infoBlock={liftInfo} filterValues={filterValues} />
+        <SkiAreaCardTitle title={name} onClick={toggleIsExpanded} isExpanded={isExpanded} />
+        <div className={expandableClassNames}>
+          <SkiAreaCardSubTitle title={'Snow Info'} />
+          <InfoBlock infoBlock={snowInfo} filterValues={filterValues} />
+          <SkiAreaCardSubTitle title={'Lift Info'} />
+          <InfoBlock infoBlock={liftInfo} filterValues={filterValues} />
+        </div>
       </div>
   )
 }
